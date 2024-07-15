bfbf215043fccab563e3a9589c98e8a67ca6b013
aripTaz <121712805+aripTaz@users.noreply.github.com>
2024-07-15 09:27:32 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -579,7 +579,7 @@ TEXT="
 <code>EXP SCRIPT : </code><code>$EXP Days</code>
 <code>━━━━━━━━━━━━━━━━━━━━</code>
 <i> Notifikasi Installer Script...</i>
-"'&reply_markup={"inline_keyboard":[[{"text":"ᴏʀᴅᴇʀ","url":"https://t.me/VpsB4ruu"},{"text":"GRUP","url":"https://t.me/satanofficiall"}]]}'
+"'&reply_markup={"inline_keyboard":[[{"text":"ᴏʀᴅᴇʀ","url":"https://t.me/VpsB4ruu"},{"text":"GRUP","url":"https://t.me/VpsB4ruu"}]]}'
 curl -s --max-time $TIMES -d "chat_id=$CHATID&disable_web_page_preview=1&text=$TEXT&parse_mode=html" $URL >/dev/null
 clear
 }
