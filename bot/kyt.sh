b3d7eedafd07b60fb05c82f98efff8cef299787e
aripTaz <121712805+aripTaz@users.noreply.github.com>
2024-07-17 01:29:58 +0700

    Update kyt.sh

--- bot/kyt.sh
+++ bot/kyt.sh
@@ -19,13 +19,13 @@ apt update && apt upgrade
 apt install neofetch -y
 apt install python3 python3-pip git
 cd /usr/bin
-wget https://raw.githubusercontent.com/aripTaz/mysc/main/Bot/bot.zip
+wget https://raw.githubusercontent.com/aripTaz/mysc/main/bot/bot.zip
 unzip bot.zip
 mv bot/* /usr/bin
 chmod +x /usr/bin/*
 rm -rf bot.zip
 clear
-wget https://raw.githubusercontent.com/aripTaz/mysc/main/Bot/kyt.zip
+wget https://raw.githubusercontent.com/aripTaz/mysc/main/bot/kyt.zip
 unzip kyt.zip
 pip3 install -r kyt/requirements.txt
 
