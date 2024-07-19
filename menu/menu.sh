f1f3dcad9aac8e2b6dd476ad38c0a34bfb544de4
aripTaz <121712805+aripTaz@users.noreply.github.com>
2024-07-19 11:20:53 +0700

    Update menu.sh

--- menu/menu.sh
+++ menu/menu.sh
@@ -305,7 +305,7 @@ clear
 clear && clear && clear
 clear;clear;clear
 echo -e " $COLOR1╭══════════════════════════════════════════════════════════╮${NC}"
-echo -e " $COLOR1│${NC} ${COLBG1}                ${WH} • SFVT VPN TUNNELING •                 ${NC} $COLOR1│ $NC"
+echo -e " $COLOR1│${NC} ${COLBG1}                ${WH} • SILA VPN TUNNELING •                 ${NC} $COLOR1│ $NC"
 echo -e " $COLOR1╰══════════════════════════════════════════════════════════╯${NC}"
 echo -e " $COLOR1╭══════════════════════════════════════════════════════════╮${NC}"
 echo -e " $COLOR1│$NC${WH} ❈ OS            ${COLOR1}: ${WH}$MODEL2${NC}"
